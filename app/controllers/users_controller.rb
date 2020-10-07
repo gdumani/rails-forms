193f0ca86f6af58c1d36597f54c51d031eb19f55
gdumani <gdumani@gmail.com>
2020-10-07 13:52:57 -0600

    improved forms to user hashed parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+   # @user = User.new(params[:user])
+    @user = User.new(user_params)
   
     if @user.save
       redirect_to new_user_path
@@ -12,4 +13,9 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def user_params
+    params.require(:user).permit(:username,:password,:email)
+  end
+
 end
